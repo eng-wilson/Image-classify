c802283d61fbd03d1d88d5e673524ffdff61db81
Wilson Santos Carvalho <wilson@Mac-mini-de-Wilson.local>
2019-11-23 14:03:35 -0300

    api endpoint atualizada

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   //API_ENDPOINT: 'http://localhost:5000/api',
-  API_ENDPOINT: 'https://lucid-cray-84ad58.netlify.com/'
+  API_ENDPOINT: 'https://lucid-cray-84ad58.netlify.com/api'
 };
